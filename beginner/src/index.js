11636f101b77faa4aee7d18fd0f6fa1cbc396e73
rajinwonderland <raj@novvuminc.com>
2018-10-21 12:59:20 -0700

    Updates to new graph.cool alias

--- beginner/src/index.js
+++ beginner/src/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import './styles.css';
 
 const client = new ApolloClient({
-	uri: 'https://api.graph.cool/simple/v1/cjni7fpei42as01327wl5m739'
+	uri: 'https://api.graph.cool/simple/v1/hero-project-beginner'
 });
 
 const App = () => (
